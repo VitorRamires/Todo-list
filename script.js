0a5c0f822af289d9242588d79ded8d7ccf0bdfd2
VitorRamires <vmr.rs@hotmail.com>
2023-12-11 16:47:12 -0300

    adicionar, remover e editar

--- script.js
+++ script.js
@@ -1,22 +1,64 @@
 
-const tarefasAtivas = document.querySelector('.tarefas-ativas')
-const botaoAddTarefa = document.querySelector('.adicionar')
-const inputAddTarefa = document.querySelector('#addTarefa')
+const tarefasAtivas = document.querySelector('.tarefas-ativas'),
+      tarefasCompletas = document.querySelector('.tarefas-completas')
+      botaoAddTarefa = document.querySelector('.adicionar'),
+      inputAddTarefa = document.querySelector('#addTarefa')
+      botaoLimpar = document.querySelector('.limpar')
 
 function adicionandoTarefa(){
   if(inputAddTarefa.value === ""){
     alert('Preencha o campo')
   } else {
-    let tarefa = document.createElement('p')
-    tarefa.innerText = inputAddTarefa.value
+    let tarefa = document.createElement('div'),
+        descricaoTarefa = document.createElement('input')
+    
     tarefasAtivas.appendChild(tarefa)
+    tarefa.appendChild(descricaoTarefa)
+    descricaoTarefa.value = inputAddTarefa.value
+   
+    tarefa.classList.add('tarefa')
+    descricaoTarefa.classList.add('descricao-tarefa')
+
+    let botoes = document.createElement('div')
+    botoes.classList.add('botoes')
+    tarefa.appendChild(botoes)
 
     const removerTarefa = document.createElement('a')
-    removerTarefa.innerText = '    remover    '
-    tarefa.appendChild(removerTarefa)
+    botoes.appendChild(removerTarefa)
+    removerTarefa.classList.add('botao-remover')
     removerTarefa.onclick = ()=>{
       tarefasAtivas.removeChild(tarefa)
     }
+
+    const divCompletarTarefa = document.createElement('div'),
+          completarTarefa = document.createElement('input'),
+          botaoCompletar = document.createElement('div')
+
+    
+    divCompletarTarefa.appendChild(completarTarefa)
+    divCompletarTarefa.appendChild(botaoCompletar)
+    botoes.appendChild(divCompletarTarefa)
+    
+    completarTarefa.setAttribute('type', 'checkbox')
+    botaoCompletar.classList.add('botao-completar')
+    divCompletarTarefa.classList.add('completar-box')
+  
+    completarTarefa.onclick = ()=>{
+      if(completarTarefa.checked === true){
+        tarefa.classList.add('completa')
+        descricaoTarefa.setAttribute('disabled', true)
+        tarefasCompletas.appendChild(tarefa)
+        
+      } else {
+        tarefa.classList.remove('completa')
+        descricaoTarefa.removeAttribute('disabled')
+      }
+    }
+    
+    botaoLimpar.addEventListener('click', ()=>{
+      tarefasAtivas.removeChild(tarefa)
+      tarefasCompletas.removeChild(tarefa)
+    })
   }
 }
 
