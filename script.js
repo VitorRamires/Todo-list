bfd708e8db53d7d21d0c6d5912cd84833e6905a7
VitorRamires <vmr.rs@hotmail.com>
2023-12-27 18:21:55 -0300

    reorganizando código

--- script.js
+++ script.js
@@ -1,159 +1,125 @@
-const painelTarefas = document.querySelector('.painel-tarefas'),
-      tarefasAtivas = document.querySelector('.tarefas-ativas'),
-      tarefasCompletas = document.querySelector('.tarefas-completas')
-      botaoAddTarefa = document.querySelector('.adicionar'),
-      inputAddTarefa = document.querySelector('#addTarefa'),
-      botaoLimpar = document.querySelector('.limpar'),
-      createCaracterCount = document.querySelector('.caracter-count'),
-      numeroAtivas = document.querySelector('.numero-ativas')
+const painelTarefas = document.querySelector('.painel-tarefas')
+const tarefasAtivas = document.querySelector('.tarefas-ativas')
+const tarefasCompletas = document.querySelector('.tarefas-completas')
+const botaoAddTarefa = document.querySelector('.adicionar')
+const inputAddTarefa = document.querySelector('#addTarefa')
+const botaoLimpar = document.querySelector('.limpar')
+const caracterCount = document.querySelector('.caracter-count')
 
 
-
-
-function adicionandoTarefa(event){
-  if(inputAddTarefa.value === "" || inputAddTarefa.value.length > 20){
-    alert('Preencha o campo ou respeite o limite de caracteres')
+function criarTarefa(){
+  if(inputAddTarefa.value === '' || inputAddTarefa.value.length > 20){
+      alert('Preencha o campo corretamente')
   } else {
-    let tarefa = document.createElement('div'),
-        descricaoTarefa = document.createElement('input')
-        
+    let createTarefa = document.createElement('div')
+    let createTexto = document.createElement('input')
+    let createExtras = document.createElement('div')
 
-    tarefasAtivas.appendChild(tarefa)
-    tarefa.appendChild(descricaoTarefa)
-    descricaoTarefa.value = inputAddTarefa.value
-    descricaoTarefa.disabled = true
-   
-    tarefa.classList.add('tarefa', 'entrada-animada')
-    descricaoTarefa.classList.add('descricao-tarefa')
+    createTarefa.classList.add("box-tarefa")
+    createTexto.classList.add("texto-tarefa")
+    createExtras.classList.add("botoes-tarefa")
 
-    let botoes = document.createElement('div')
-    botoes.classList.add('botoes')
-    tarefa.appendChild(botoes)
+    createTexto.disabled = true
 
-
-    const divCompletarTarefa = document.createElement('div'),
-          completarTarefa = document.createElement('input'),
-          botaoCompletar = document.createElement('div'),
-          verificarRemover = document.createElement('div')
-
-   
-    divCompletarTarefa.appendChild(completarTarefa)
-    divCompletarTarefa.appendChild(botaoCompletar)
-    botoes.appendChild(verificarRemover)
-
-    verificarRemover.appendChild(divCompletarTarefa)
-    verificarRemover.classList.add('verificar-remover')
+    createTarefa.appendChild(createTexto)
+    createTarefa.appendChild(createExtras)
+    tarefasAtivas.appendChild(createTarefa)
     
-    completarTarefa.setAttribute('type', 'checkbox')
-    botaoCompletar.classList.add('botao-completar')
-    divCompletarTarefa.classList.add('completar-box')
-
-
-    completarTarefa.onclick = ()=>{
-      if(completarTarefa.checked === true){
-        tarefa.classList.add('completar')
-        descricaoTarefa.setAttribute('disabled', true)
-        tarefasCompletas.appendChild(tarefa)
-      } else {
-        tarefa.classList.remove('completar')
-        descricaoTarefa.removeAttribute('disabled')
-        tarefasAtivas.appendChild(tarefa)
-      }
-    }
+    createTexto.value = inputAddTarefa.value
     
-  
+    createExtras.appendChild(completarTarefa(createTarefa))
+    createExtras.appendChild(removerTarefa(createTarefa))
+    createExtras.appendChild(editarTarefa(createTarefa, createTexto))
 
-    const removerTarefa = document.createElement('a')
-    verificarRemover.appendChild(removerTarefa)
-    removerTarefa.classList.add('botao-remover')
-    removerTarefa.onclick = ()=>{
-      if(tarefa.classList.contains('completar')){
-        tarefasCompletas.removeChild(tarefa)
-      } else {
-        tarefasAtivas.removeChild(tarefa)
-      }
-    }
-
-    let todasTarefas = document.querySelectorAll('.tarefa')
-    botaoLimpar.onclick = ()=>{
-      todasTarefas.forEach(tarefaItem=>{
-        if(tarefaItem.classList.contains('completar')){
-          tarefasCompletas.removeChild(tarefaItem)
-        } else {
-          tarefasAtivas.removeChild(tarefaItem)
-        }
-      })
-    }
-
-
-    const valorInputSalvo = descricaoTarefa.value
-    let botaoEditar = document.createElement('div')
-    botaoEditar.classList.add('editar')
-    verificarRemover.appendChild(botaoEditar)
-    
-    botaoEditar.onclick = ()=>{
-      const salvarCancelar = document.createElement('div'),
-            salvar = document.createElement('a'),
-            cancelar = document.createElement('a')
-
-      descricaoTarefa.disabled = false
-      descricaoTarefa.focus()
-      descricaoTarefa.setSelectionRange(descricaoTarefa.value.length, descricaoTarefa.value.length)
-
-      verificarRemover.classList.add('esconder-botoes')
-      salvarCancelar.classList.add('salvar-cancelar')
-      botoes.appendChild(salvarCancelar)
-
-      salvar.innerHTML = 'Salvar'
-      cancelar.innerHTML = 'Cancelar'
-
-      salvar.classList.add('salvar')
-      cancelar.classList.add('cancelar')
-
-      salvarCancelar.appendChild(salvar)
-      salvarCancelar.appendChild(cancelar)
-
-      salvar.addEventListener('click', ()=>{
-        if(descricaoTarefa.value === '' || descricaoTarefa.value.length > 20){
-          alert('preencha o campo de edição corretamente')
-        } else {
-          verificarRemover.classList.remove('esconder-botoes')
-          salvarCancelar.classList.add('esconder-botoes')
-          descricaoTarefa.disabled = true
-        }
-      })
-
-      cancelar.addEventListener('click', ()=>{
-        descricaoTarefa.value = valorInputSalvo
-        verificarRemover.classList.remove('esconder-botoes')
-        salvarCancelar.classList.add('esconder-botoes')
-        descricaoTarefa.disabled = true
-      })
-    }
-
-
-
-
-
+    inputAddTarefa.value = ''
+    caracterCount.innerHTML = 0 + " / 20"
   }
 }
 
+function completarTarefa(tarefa){
+  let boxCompletar = document.createElement('div')
+  let botaoCompletar = document.createElement('input')
+  let botaoCompletarPersonalizado = document.createElement('div')
+  
+  botaoCompletar.setAttribute('type', 'checkbox')
+  boxCompletar.classList.add('box-completar')
+  botaoCompletar.classList.add('botao-completar')
+  botaoCompletarPersonalizado.classList.add('personalizar-completar')
+  
+  boxCompletar.appendChild(botaoCompletar)
+  boxCompletar.appendChild(botaoCompletarPersonalizado)
 
-
-botaoAddTarefa.addEventListener('click', (event)=>{
-  adicionandoTarefa(event)
-  inputAddTarefa.value = ''
-  createCaracterCount.innerHTML = 0
-  createCaracterCount.classList.add('disable-count')
-})
-inputAddTarefa.addEventListener('keyup', (event)=>{
-  createCaracterCount.innerHTML = + inputAddTarefa.value.length + " / 20"
-  inputAddTarefa.value.length === 0 ? createCaracterCount.classList.add('disable-count') : createCaracterCount.classList.remove('disable-count')
-  inputAddTarefa.value.length > 20 ? createCaracterCount.classList.add('caracter-ok') : createCaracterCount.classList.remove('caracter-ok')
-  if(event.key === "Enter"){
-    adicionandoTarefa()
-    inputAddTarefa.value = ''
-    createCaracterCount.innerHTML = 0
-    createCaracterCount.classList.add('disable-count')
+  botaoCompletar.onclick = ()=>{
+    if(botaoCompletar.checked === false){
+      tarefasAtivas.appendChild(tarefa)
+      tarefa.classList.remove('completada')
+    } else if (botaoCompletar.checked === true){
+      tarefasCompletas.appendChild(tarefa)
+      tarefa.classList.add('completada')
+    }
   }
-})
+  return boxCompletar
+}
+
+function removerTarefa(tarefa){
+  let botaoRemover = document.createElement('div')
+  botaoRemover.classList.add('botao-remover')
+
+  botaoRemover.onclick = ()=>{
+    if(tarefa.classList.contains('completada')){
+      tarefasCompletas.removeChild(tarefa)
+    } else {
+      tarefasAtivas.removeChild(tarefa)
+    }
+  }
+  return botaoRemover
+}
+
+function editarTarefa(createTarefa, createTexto){
+  let botaoEditar = document.createElement('div')
+  botaoEditar.classList.add('botao-editar')
+  botaoEditar.onclick = ()=>{
+    if(createTexto.value === '' || createTexto.value.length > 20){
+      alert('Preencha o campo corretamente')
+    } else {
+      if(createTexto.disabled === false){
+        createTexto.disabled = true
+        botaoEditar.classList.remove('salvar-edicão')
+        createTarefa.classList.remove('destacar-tarefa-modificada')
+      } else if (createTexto.disabled === true) {
+        createTexto.disabled = false
+        botaoEditar.classList.add('salvar-edicão')
+        createTarefa.classList.add('destacar-tarefa-modificada')
+        createTexto.focus()
+        createTexto.setSelectionRange(createTexto.value.length, createTexto.value.length)
+      }
+    }
+  }
+  return botaoEditar
+}
+
+function limparPainelTarefas(){
+  const todasTarefas = document.querySelectorAll('.box-tarefa')
+  todasTarefas.forEach(tarefa=>{
+    if(tarefa.classList.contains('completada')){
+      tarefasCompletas.removeChild(tarefa)
+    } else {
+      tarefasAtivas.removeChild(tarefa)
+    }
+  })
+}
+
+
+function keyboardHandler(event){
+  caracterCount.innerHTML = inputAddTarefa.value.length + " / 20"
+  inputAddTarefa.value.length > 25 ? caracterCount.classList.add('caracter-ok') : caracterCount.classList.remove('caracter-ok')
+  if(event.key === "Enter"){
+    criarTarefa()
+    inputAddTarefa.value = ''
+    caracterCount.innerHTML = 0 + " / 20"
+  } 
+}
+
+botaoAddTarefa.addEventListener('click', criarTarefa)
+botaoLimpar.addEventListener('click', limparPainelTarefas)
+inputAddTarefa.addEventListener('keyup', keyboardHandler)
